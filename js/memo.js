bdab57ed56593ca4125d5fa59d172d356a613db0
Tomasz Bochenek <bochenekt@gmail.com>
2018-07-16 22:48:13 +0200

    feat: add dealCards() function which assigns icons to cards

--- js/memo.js
+++ js/memo.js
@@ -6,10 +6,10 @@ const cardsDeck = ["fa-at", "fa-at", "fa-beer", "fa-beer",
 
 
 /**
-*Function to random shuffle card-front icon's classes
-*(based on function in starter code)
-*@param {array} cardsDeck - cards icon's classes
-*@return {array} cardsDeck - shuffled array
+* Function to random shuffle card-front icon's classes
+* (based on function in starter code)
+* @param {array} cardsDeck - cards icon's classes
+* @return {array} cardsDeck - shuffled array
 */
 function shuffle(cardsDeck) {
 	let currentIndex = cardsDeck.length, temporaryValue, randomIndex;
@@ -23,4 +23,17 @@ function shuffle(cardsDeck) {
 	} 
 	
 	return cardsDeck;
+}
+
+/**
+* Function to add shuffled classes to cards
+*/
+function dealCards() {
+	// create array of i elements which represents cards
+	const cardsList = document.getElementsByClassName('fas');
+	// add icon-class for each i element
+	for (i = 0; i < cardsList.length; i++) {
+		let addIcon = cardsList[i];
+		addIcon.classList.add(cardsDeck[i]);
+	} 
 }
